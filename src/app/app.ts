dfa8d79febbbdaf76b48f5ac433219730cae485a
Timothy Mazenge <timmazenge@gmail.com>
2026-01-01 09:44:13 -0800

    feat: implement calendar skeleton

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Calendar } from './calendar/calendar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , Calendar],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
 export class App {
   protected readonly title = signal('erp');
